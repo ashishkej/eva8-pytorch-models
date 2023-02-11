6816fae52d3b90eea1ab466b4b8772b107939d2c
ashishkej <ashishkej@outlook.com>
2023-02-11 23:27:09 +0530

    augmentation class

--- utils.py
+++ utils.py
@@ -224,3 +224,32 @@ def get_mean_and_std(dataset):
     std.div_(len(dataset))
     return mean, std
 
+class AlbumentationImageDataset(Dataset):
+  def __init__(self, image_list, train= True):
+      self.image_list = image_list
+      self.aug = A.Compose({
+          A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+          A.HorizontalFlip(),
+          A.ShiftScaleRotate(),
+          A.CoarseDropout(1, 16, 16, 1, 16, 16,fill_value=0.473363, mask_fill_value=None),
+          A.ToGray()
+      })
+
+      self.norm = A.Compose({A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+      })
+      self.train = train
+        
+  def __len__(self):
+      return (len(self.image_list))
+
+  def __getitem__(self, i):
+      
+      image, label = self.image_list[i]
+      
+      if self.train:
+        #apply augmentation only for training
+        image = self.aug(image=np.array(image))['image']
+      else:
+        image = self.norm(image=np.array(image))['image']
+      image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+      return torch.tensor(image, dtype=torch.float), label
