33bbf51276bb15fba457f413802edb1f0b7bcf2d
ashishkej <ashishkej@outlook.com>
2023-02-12 17:03:14 +0530

    gradcam vis

--- utils.py
+++ utils.py
@@ -6,26 +6,29 @@ from torchvision.utils import make_grid
 
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
-from torchvision.models import resnet50
 
-import torch 
 from torch.utils.data import Dataset
 
-import cv2
 
 
+def gradcam_vis(model,target_layers, misclf, classes,plot_size=(4,5)):
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
+    rows, cols = plot_size
+    figure = plt.figure(figsize=(cols*3,rows*3))
 
-def gradcam_vis(model,target_layers, input_tensor, target_class, rgb_img):
-    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda)
-    targets = [ClassifierOutputTarget(target_class)]
-    grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
-    grayscale_cam = grayscale_cam[0, :]
-    fig, (ax1, ax2) = plt.subplots(figsize=(20,20), ncols=2)
+    for i in range(1, cols * rows + 1):
+        img_model, img, label, pred = misclf[i-1]
+        targets = [ClassifierOutputTarget(label)]
+        grayscale_cam = cam(input_tensor=img_model.unsqueeze(0), targets=targets)
+        grayscale_cam = grayscale_cam[0, :]
+        figure.add_subplot(rows, cols, i) # adding sub plot
+        plt.title(f"Pred label: {classes[pred]}\n True label: {classes[label]}") # title of plot
+        plt.axis("off") # hiding the axis
+        plt.imshow(img) # showing the plot
+        plt.imshow(grayscale_cam, cmap='magma', alpha=0.65)
 
-    ax1.imshow(rgb_img)
-    ax2.imshow(rgb_img)
-    ax2.imshow(grayscale_cam, cmap='magma', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
 
 
 def convert_image_np(inp, mean, std):
@@ -46,64 +49,54 @@ def convert_image_np(inp, mean, std):
     return inp
 
 
-def plot_data(data, rows, cols, lower_value, upper_value):
+def plot_data(data, rows, cols):
     """Randomly plot the images from the dataset for vizualization
 
     Args:
         data (instance): torch instance for data loader
         rows (int): number of rows in the plot
         cols (int): number of cols in the plot
-        lower_value (int): lower value of the dataset for plotting in a particular interval. 0 for starting index
-        upper_value (int): upper value for plotting in a particular interval. len of dataset for last index index
     """
     figure = plt.figure(figsize=(cols*2,rows*3))
     for i in range(1, cols*rows + 1):
-        k = np.random.randint(lower_value,upper_value)
+        k = np.random.randint(0,50000)
         figure.add_subplot(rows, cols, i) # adding sub plot
 
-        img, label = data.dataset[k]
+        img, label = data[k]
         
         plt.imshow(img)
         plt.axis('off')
-        plt.title(f'Class: {label}')
+        plt.title(f'Class: {label} '+data.classes[label])
 
     plt.tight_layout()
     plt.show()
 
 
-def plot_aug(aug_dict, data, ncol=6):
-    """Vizualize the image for the augmentations to be applied over dataset
+def show_aug_images(exp, aug_dict, ncol=6):
+  nrow = len(aug_dict)
 
-    Args:
-        aug_dict (dict): dictionary key as name of augmentation to applied (str) and 
-                                    value as albumentations aug function
-        data (instance): torch instance for data loader
-        ncol (int, optional): number of cols in the plot. Defaults to 6.
-    """
-    nrow = len(aug_dict)
+  fig, axes = plt.subplots(ncol, nrow, figsize=( 3*nrow, 15), squeeze=False)
+  for i, (key, aug) in enumerate(aug_dict.items()):
+    for j in range(ncol):
+      ax = axes[j,i]
+      if j == 0:
+        ax.text(0.5, 0.5, key, horizontalalignment='center', verticalalignment='center', fontsize=15)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.axis('off')
+      else:
+        image, label = exp[j-1]
+        if aug is not None:
+          transform = A.Compose([aug])
+          image = np.array(image)
+          image = transform(image=image)['image']
+          
+        ax.imshow(image)
+        ax.set_title(f'{exp.classes[label]}')
+        ax.axis('off')
 
-    fig, axes = plt.subplots(ncol, nrow, figsize=( 3*nrow, 15), squeeze=False)
-    for i, (key, aug) in enumerate(aug_dict.items()):
-        for j in range(ncol):
-            ax = axes[j,i]
-            if j == 0:
-                ax.text(0.5, 0.5, key, horizontalalignment='center', verticalalignment='center', fontsize=15)
-                ax.get_xaxis().set_visible(False)
-                ax.get_yaxis().set_visible(False)
-                ax.axis('off')
-            else:
-                image, label = data.dataset[j-1]
-                if aug is not None:
-                    transform = A.Compose([aug])
-                    image = np.array(image)
-                    image = transform(image=image)['image']
-                
-                ax.imshow(image)
-                #ax.set_title(f'{data.classes[label]}')
-                ax.axis('off')
-
-    plt.tight_layout()
-    plt.show()
+  plt.tight_layout()
+  plt.show()
     
 
 def plot_misclassified(model, test_loader, classes, device, dataset_mean, dataset_std, no_misclf=20, plot_size=(4,5), return_misclf=False):
@@ -133,23 +126,23 @@ def plot_misclassified(model, test_loader, classes, device, dataset_mean, datase
     misclf = list()
   
     while count<no_misclf:
-        img, label = test_loader.dataset[k]
-        pred = model(img.unsqueeze(0).to(device)) # Prediction
+        img_model, label = test_loader.dataset[k]
+        pred = model(img_model.unsqueeze(0).to(device)) # Prediction
         # pred = model(img.unsqueeze(0).to(device)) # Prediction
         pred = pred.argmax().item()
 
         k += 1
         if pred!=label:
             img = convert_image_np(
-                img, dataset_mean, dataset_std)
-            misclf.append((img, label, pred))
+                img_model, dataset_mean, dataset_std)
+            misclf.append((img_model, img, label, pred))
             count += 1
     
     rows, cols = plot_size
     figure = plt.figure(figsize=(cols*3,rows*3))
 
     for i in range(1, cols * rows + 1):
-        img, label, pred = misclf[i-1]
+        _, img, label, pred = misclf[i-1]
 
         figure.add_subplot(rows, cols, i) # adding sub plot
         plt.title(f"Pred label: {classes[pred]}\n True label: {classes[label]}") # title of plot
@@ -161,54 +154,6 @@ def plot_misclassified(model, test_loader, classes, device, dataset_mean, datase
     
     if return_misclf:
         return misclf
-
-
-def plot_learning_curves(history=None, from_txt=False, plot_lr_trend=False):
-    """Plot Test & Train Learning Curves of model
-
-    Args:
-        history (tuple, optional): tuple of list contraing (training_acc, training_loss, testing_acc, testing_loss)
-                            Note- in specific order only. Defaults to None. if information is dumped to txt file
-        from_txt (bool or list, optional): List of path to (training_acc, training_loss, testing_acc, testing_loss) txt files
-                            Note- give path in specific order only. Defaults to False.
-        plot_lr_trend (bool or list or str, optional): List if plot learning curve trend form list 
-                                                       str- plot from txt file, path to txt file. 
-                                                       Defaults to False. (dont plot)
-    """
-    if from_txt:
-        history = []
-        for path in from_txt:
-            history.append([float(i) for i in open(path).read().strip().split("\n")])
-    
-
-    fig, axs = plt.subplots(1,2,figsize=(16,7))
-    axs[0].set_title('LOSS')
-    axs[0].plot(history[1], label='Train')
-    axs[0].plot(history[3], label='Test')
-    axs[0].legend()
-    axs[0].grid()
-
-    axs[1].set_title('Accuracy')
-    axs[1].plot(history[0], label='Train')
-    axs[1].plot(history[2], label='Test')
-    axs[1].legend()
-    axs[1].grid()
-
-    plt.show()
-
-    if plot_lr_trend:
-        if from_txt:
-            lr_trend = [float(i) for i in open(plot_lr_trend).read().strip().split("\n")]
-        else:
-            lr_trend = plot_lr_trend
-        
-        plt.plot(lr_trend)
-        plt.title('Learning Rate Change During Training')
-        plt.xlabel('Iteration')
-        plt.ylabel('LR')
-        plt.grid()
-        plt.show()
-
 
 def get_mean_and_std(exp_data):
     '''Calculate the mean and std for normalization'''
